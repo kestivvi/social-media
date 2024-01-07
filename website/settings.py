3bea2348a8e7211383f5b6f7f976a9e794a06bb4
Maksio <jozef@gorowo.pl>
2024-01-07 21:13:40 +0100

    Poprawiono wyświetlanie przycisków

--- website/settings.py
+++ website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9npc35e5)-3nog*zd4l((=f%hrd2sjgzv#x5=3tr=k-(^_tm*+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
