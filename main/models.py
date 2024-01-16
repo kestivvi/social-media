f676630b8c12910fc397d7e1c5207435c09ad244
Zetka <jakub.kochanski10@gmail.com>
2024-01-16 23:15:59 +0100

    added likes to post model

--- main/models.py
+++ main/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return self.title + "\n" + self.description
