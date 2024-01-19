579d077dd40047a9239d236239d65ef7b85bbcd7
Zetka <jakub.kochanski10@gmail.com>
2024-01-19 23:16:40 +0100

    added create and delete comment methods

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("sign-up", views.sign_up, name="sign_up"),
     path("create-post", views.create_post, name="create_post"),
     path("logout/", logout_view, name="logout"),
+    path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
